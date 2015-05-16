33ec539975e13c31db44c82c83a970bb7756bf18
s8k10 <s8.kato@gmail.com>
2015-05-16 23:22:24 +0900

    branchcommit

--- GitHubTestRepo/Form1.cs
+++ GitHubTestRepo/Form1.cs
@@ -21,5 +21,10 @@ namespace GitHubTestRepo
         {
             MessageBox.Show("Hello, GitHub.");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("ブランチする");
+        }
     }
 }
