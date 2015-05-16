ec0f2331171a75fff05b01500552dd2bcbc573e8
s8k10 <s8.kato@gmail.com>
2015-05-16 23:16:57 +0900

    HelloGitHub

--- GitHubTestRepo/Form1.cs
+++ GitHubTestRepo/Form1.cs
@@ -16,5 +16,10 @@ namespace GitHubTestRepo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, GitHub.");
+        }
     }
 }
